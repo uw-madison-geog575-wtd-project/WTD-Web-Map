50422798476d2ff03c832bc1d71c5034cb3ef292
pjbernard-uwmadison <pjbernard110@gmail.com>
2019-04-24 20:43:34 -0500

    setting up the clustering -pb
    linking to the css and js in the lib and creating the code to get the clustering working.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var L;
+/*var L;
 var $;
 
 //initialize map
@@ -24,4 +24,39 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 getData(map);
 
 
-$(document).ready(Map);
+$(document).ready(Map);*/
+
+function createMap(){
+    
+    var map = L.map('map', {
+    center: [0, 0],
+    zoom: 3,
+    minZoom: 1,
+    maxZoom: 18
+    });
+
+
+    var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.dark',
+    accessToken: 'pk.eyJ1Ijoiam15YXR0IiwiYSI6ImNqbG9vMnc4MjA5ZTczcHBiYmlzYTNhcDAifQ.BaYtqvvn4Lzsl6mdotKeLQ'
+    }).addTo(map);
+
+
+    getData(map);
+};
+
+function getData(map){
+    $.ajax("data/terrorData1970.json", {
+        dataType: "json",
+        success: function(response){
+            var geoJsonLayer = L.geoJson(response);
+            var markers = L.markerClusterGroup();
+            markers.addLayer(geoJsonLayer);
+            map.addLayer(markers);
+        }
+    });
+};
+
+$(document).ready(createMap);
