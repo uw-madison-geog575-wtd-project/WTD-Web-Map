69fa283b2511bbd39f69779d58802f635bfb8df3
pjbernard-uwmadison <pjbernard110@gmail.com>
2019-04-24 20:53:54 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,19 +29,19 @@ $(document).ready(Map);*/
 function createMap(){
     
     var map = L.map('map', {
-    center: [0, 0],
-    zoom: 3,
-    minZoom: 1,
-    maxZoom: 18
-    });
+        center: [0, 0],
+        zoom: 3,
+        minZoom: 1,
+        maxZoom: 18
+        });
 
 
     var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.dark',
-    accessToken: 'pk.eyJ1Ijoiam15YXR0IiwiYSI6ImNqbG9vMnc4MjA5ZTczcHBiYmlzYTNhcDAifQ.BaYtqvvn4Lzsl6mdotKeLQ'
-    }).addTo(map);
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.dark',
+        accessToken: 'pk.eyJ1Ijoiam15YXR0IiwiYSI6ImNqbG9vMnc4MjA5ZTczcHBiYmlzYTNhcDAifQ.BaYtqvvn4Lzsl6mdotKeLQ'
+        }).addTo(map);
 
 
     getData(map);
