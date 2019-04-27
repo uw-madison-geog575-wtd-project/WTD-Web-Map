4022eb924a74a7a3bfb9ff9b7f421c583f95abd4
pjbernard-uwmadison <pjbernard110@gmail.com>
2019-04-26 19:17:14 -0500

    add d3 to lib

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 (function(){
 
+//pseudo-global variables and the initial expressed attribute
+var attrArray = ["YR2000","YR2001","YR2002","YR2003","YR2004","YR2005","YR2006","YR2007","YR2008","YR2009","YR2010","YR2011","YR2012","YR2013","YR2014","YR2015","YR2016","YR2017"],
+    expressed = attrArray[0];
+
+  
 function createMap(){
     
     var map = L.map('map', {
