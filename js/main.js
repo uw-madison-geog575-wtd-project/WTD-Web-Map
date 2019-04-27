f01c1a558f14613b43b9f6bb94c639a3906cefcc
pjbernard-uwmadison <pjbernard110@gmail.com>
2019-04-27 16:22:34 -0500

    top 10 fixes/map refocus -pb

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 //pseudo-global variables and the initial expressed attribute
 var attrArray = ["YR2000","YR2001","YR2002","YR2003","YR2004","YR2005","YR2006","YR2007","YR2008","YR2009","YR2010","YR2011","YR2012","YR2013","YR2014","YR2015","YR2016","YR2017"],
-    expressed = attrArray[17];
+    expressed = attrArray[0];
     console.log(expressed);
   
 function createMap(){
     
     var map = L.map('map', {
-        center: [0, 0],
-        zoom: 3,
+        center: [52,12],
+        zoom: 4,
         minZoom: 1,
         maxZoom: 18
         });
@@ -88,18 +88,23 @@ function updateTopTen(ByYear_CountryCsv){
     
     console.log(topTenArray[5]);
     
-    var topTenCountries = []
+    var topTenCountries = [],
+        duplicateCheck = [];
     
     for (var i=0; i < 10; i++){
-        var arrayToPush = [],
-            findMatch = topTenArray[i];
+        var findMatch = topTenArray[i];
         for (var a=0; a<ByYear_CountryCsv.length; a++){
-            if (ByYear_CountryCsv[a][expressed]==findMatch){
+            if (ByYear_CountryCsv[a][expressed]==findMatch && !duplicateCheck.includes(ByYear_CountryCsv[a].COUNTRY)){
                 //console.log(ByYear_CountryCsv[a].COUNTRY);
                 topTenCountries.push([ByYear_CountryCsv[a].COUNTRY,findMatch]);
+                duplicateCheck.push(ByYear_CountryCsv[a].COUNTRY);
             }
         }
     }
+    while (topTenCountries.length != 10){
+        topTenCountries.pop();
+    }
+        
     console.log(topTenCountries);
     
     for (var i=0; i<topTenCountries.length; i++){
